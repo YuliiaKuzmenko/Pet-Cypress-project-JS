1786c326b5101356b584e9b58de7cfcadf0d35c6
Yuliia-Kuzmenko <kuzmenkoyuliia22@gmail.com>
2024-11-29 15:01:59 -0800

    fixes-week-7

---
+++ week-6.1-Automation/homework.cy.js
@@ -0,0 +1,38 @@
+
+// Challenge 1: Find the Largest Palindrome
+// Write a function to find the largest palindrome made from the product of any two numbers between 0 and 1000
+// Usage examples:
+// console.log(getLargestPalindrome(1000));; // 906609
+
+// function getLargestPalindrome()
+
+
+// Challenge 2: Find the most frequent element in a given array
+// Write a function to find the most frequent element in the array. If there are multiple elements that appear a maximum number of times, print the first of them
+// Usage examples:
+//let arr = [40, 50, 30, 40, 50, 20, 30, 100, 11, 11];
+
+// let n = arr.length;
+// console.log(mostFrequent(arr, n));; // 40
+
+
+// Challenge 3: Count positive and negative numbers in an array
+// Write a function to count positive and negative numbers in an array
+// Usage examples:
+//let arr = [10,20, -1,22,99,20, -9];
+// console.log(`The negative numbers in an array is ${negativeCount}`) //"The negative numbers in an array is 2"
+// console.log(`The negative numbers in an array is ${poitiveCount}`) // "The positive numbers in an array is 5"
+
+ 
+
+// Challenge 4: Find duplicates in two arrays 
+// Write a function that takes two arrays as arguments and returns an array of values that are repeated more than once across the two input arrays
+// Usage examples:
+//const array1 = [1, 2, 3, 4, 5, 5]; const array2 = [4, 5, 6, 7, 7, 8]; //console.log(findDuplicates(array1, array2)); //  [4, 5, 7]
+
+ 
+// for (let row = 0; row < 3; row++) {
+//     for (let col = 0; col < 3; col++) {
+//         console.log(Position is: (${row}, ${col}));
+//     }
+// }
